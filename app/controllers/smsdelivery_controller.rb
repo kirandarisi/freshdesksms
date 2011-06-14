c2ab3fcf028b36923efc887872935752d4af3b54
Kiran Darisi <kiran.darisi@gmail.com>
2011-06-14 15:57:31 +0530

    ome msg has been changed

--- app/controllers/smsdelivery_controller.rb
+++ app/controllers/smsdelivery_controller.rb
@@ -1,6 +1,6 @@
 class SmsdeliveryController < ApplicationController
   def create_ticket
     Freshdesk.create_tkt(params[:content])
-    render :json => {"msisdn" => params[:msisdn], "content" => "Hi We recieved your ticket we will get back to you soon!"}
+    redirect_to "/"
   end
 end
